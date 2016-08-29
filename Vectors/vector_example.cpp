3f5cfd0968a21b2995f3c652ec81a907d9b01893
Logan Yliniemi <logan@Logans-MacBook-Pro-2.local>
2016-08-29 15:07:19 -0700

    File Structure
    Adds necessary file structure for example file.

---
+++ Vectors/vector_example.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vectors
+//
+//  Created by Logan Yliniemi on 8/29/16.
+//  Copyright © 2016 Logan Yliniemi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
